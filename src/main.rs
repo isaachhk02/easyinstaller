1f5577cec89a6fcb0e6f4a6cfee3da237609836e
isaachhk02 <isaac.fornos02@gmail.com>
2025-05-17 20:19:10 +0200

    Add Arch Linux Support

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use window::installation_type;
 fn main() {
     let mut language = String::new();
     let mut args = env::args();
+    let mut its_arch;
     let path;
     if let Some(arg) = args.nth(1) {
         path = arg;
@@ -22,42 +23,57 @@ fn main() {
         }
         if path.ends_with(".deb") {
             println!("Deb file detected!");
-            installation_type(language.as_str(),"apt", &path);
+            its_arch = 0;
+            installation_type(language.as_str(), "apt", &path, its_arch);
         }
         if path.ends_with(".rpm") {
             println!("RPM file detected!");
-            installation_type(language.as_str(),"dnf", &path);
+            its_arch = 0;
+            installation_type(language.as_str(), "dnf", &path, its_arch);
+        }
+        if path.ends_with(".pkg.tar.xz") {
+            println!("PKG.TAR.XZ detected!");
+            its_arch = 1;
+            installation_type(language.as_str(), "pacman", &path, its_arch)
         }
     } else {
         println!("No arguments provided. Please provide a file path.");
-        let file_chooser: Result<Option<String>, dialog::Error> = dialog::FileSelection::new("Please select a file").mode(dialog::FileSelectionMode::Open).show();
-                println!("File chooser: {:?}", file_chooser);
-            match file_chooser {
-                Ok(file) => {
-                    if let Some(file_path) = file {
-                        path = file_path.to_string();
-                        if path.ends_with(".deb") {
-                            println!("Deb file detected!");
-                            installation_type(language.as_str(),"apt", &path);
-                        } else if path.ends_with(".rpm") {
-                            println!("RPM file detected!");
-                            installation_type(language.as_str(),"dnf", &path);
-                        } else {
-                            println!("ERROR : Please select a deb or rpm file!");
-                            return;
-                        }
+        let file_chooser: Result<Option<String>, dialog::Error> =
+            dialog::FileSelection::new("Please select a file")
+                .mode(dialog::FileSelectionMode::Open)
+                .show();
+        println!("File chooser: {:?}", file_chooser);
+        match file_chooser {
+            Ok(file) => {
+                if let Some(file_path) = file {
+                    path = file_path.to_string();
+                    if path.ends_with(".deb") {
+                        println!("Deb file detected!");
+                        its_arch = 0;
+                        installation_type(language.as_str(), "apt", &path, its_arch);
+                    } else if path.ends_with(".rpm") {
+                        println!("RPM file detected!");
+                        its_arch = 0;
+                        installation_type(language.as_str(), "dnf", &path, its_arch);
+                    } else if path.ends_with(".pkg.tar.xz") {
+                        println!("PKG.tar.xz file detected!");
+                        its_arch = 1;
+                        installation_type(language.as_str(), "pacman", &path, its_arch);
                     } else {
-                        println!("ERROR : No file selected!");
+                        println!("ERROR : Please select a deb or rpm file!");
                         return;
                     }
-
-                }
-                Err(e) => {
-                    println!("ERROR : {}", e);
+                } else {
+                    println!("ERROR : No file selected!");
                     return;
                 }
             }
-        
+            Err(e) => {
+                println!("ERROR : {}", e);
+                return;
+            }
+        }
+
         println!("Language: {}", language);
     }
 }
--- src/window.rs
+++ src/window.rs
@@ -1,50 +1,64 @@
 use dialog::backends::{Backend, Zenity};
-use dialog::{DialogBox, Message, Question, Choice};
-pub fn installation_type(language: &str, package_manager: &str, package_path: &str) {
+use dialog::{Choice, DialogBox, Message, Question};
+pub fn installation_type(language: &str, package_manager: &str, package_path: &str, its_arch: i32) {
     let uninstall;
     let install_or_remove_package_msgs = match language {
-        "en_US" => "Do you want to install or remove a package ? (press yes to install or press no to uninstall)",
+        "en_US" => {
+            "Do you want to install or remove a package ? (press yes to install or press no to uninstall)"
+        }
         "fr_FR" => "Voulez-vous installer ou supprimer un paquet?",
-        "es_ES" => "¿Quieres instalar o eliminar un paquete? (Pulsa Si para instalar el paquete de lo contrario pulsa No para eliminarlo",
-        _ => "Do you want to install or remove a package ? (press yes to install or press no to uninstall)",
+        "es_ES" => {
+            "¿Quieres instalar o eliminar un paquete? (Pulsa Si para instalar el paquete de lo contrario pulsa No para eliminarlo"
+        }
+        _ => {
+            "Do you want to install or remove a package ? (press yes to install or press no to uninstall)"
+        }
     };
 
-    let install_or_remove_package = Question::new(install_or_remove_package_msgs).title("Do you want?").show();
+    let install_or_remove_package = Question::new(install_or_remove_package_msgs)
+        .title("Do you want?")
+        .show();
     if install_or_remove_package.is_ok() {
-        
         match install_or_remove_package {
             Ok(Choice::Yes) => {
                 uninstall = 1;
-                welcome_screen(language, package_manager, package_path, uninstall);
+                welcome_screen(language, package_manager, package_path, uninstall, its_arch);
                 ()
             }
             Ok(Choice::No) | Ok(Choice::Cancel) => {
                 uninstall = 0;
-                welcome_screen(language, package_manager, package_path, uninstall);
+                welcome_screen(language, package_manager, package_path, uninstall, its_arch);
                 ()
             }
             Err(_) => {
                 uninstall = 0;
-                welcome_screen(language, package_manager, package_path, uninstall);
+                welcome_screen(language, package_manager, package_path, uninstall, its_arch);
                 ()
             }
         }
     } else {
         uninstall = 0;
-        welcome_screen(language, package_manager, package_path, uninstall);
-        }
+        welcome_screen(language, package_manager, package_path, uninstall, its_arch);
+    }
 }
 
-
-pub fn welcome_screen(language: &str, package_manager: &str, package_path: &str, uninstall: i32) {
+pub fn welcome_screen(
+    language: &str,
+    package_manager: &str,
+    package_path: &str,
+    uninstall: i32,
+    its_arch: i32,
+) {
     match package_manager {
         "apt" => "/usr/bin/apt",
         "dnf" => "/usr/bin/dnf",
+        "pacman" => "/usr/bin/pacman",
         _ => "/usr/bin/apt",
     };
     let package_path = match package_manager {
         "apt" => format!("file://{}", package_path),
         "dnf" => format!("file://{}", package_path),
+        "pacman" => format!("file://{}", package_path),
         _ => format!("file://{}", package_path),
     };
     let title = match language {
@@ -54,10 +68,18 @@ pub fn welcome_screen(language: &str, package_manager: &str, package_path: &str,
         _ => "Welcome to easyInstaller",
     };
     let message = match language {
-        "en_US" => format!("Welcome to easyInstaller\n\nThis is a simple GUI for installing packages on Linux.\n\nPlease click to install for install the specified package."),
-        "fr_FR" => format!("Bienvenue dans easyInstaller\n\nCeci est une interface graphique simple pour installer des paquets sur Linux.\n\nVeuillez cliquer pour installer le paquet spécifié."),
-        "es_ES" => format!("Bienvenido a easyInstaller\n\nEsta es una interfaz gráfica simple para instalar paquetes en Linux.\n\nHaga clic para instalar el paquete especificado."),
-        _ => format!("Welcome to easyInstaller\n\nThis is a simple GUI for installing packages on Linux.\n\nPlease click to install for install the specified package."),
+        "en_US" => format!(
+            "Welcome to easyInstaller\n\nThis is a simple GUI for installing packages on Linux.\n\nPlease click to install for install the specified package."
+        ),
+        "fr_FR" => format!(
+            "Bienvenue dans easyInstaller\n\nCeci est une interface graphique simple pour installer des paquets sur Linux.\n\nVeuillez cliquer pour installer le paquet spécifié."
+        ),
+        "es_ES" => format!(
+            "Bienvenido a easyInstaller\n\nEsta es una interfaz gráfica simple para instalar paquetes en Linux.\n\nHaga clic para instalar el paquete especificado."
+        ),
+        _ => format!(
+            "Welcome to easyInstaller\n\nThis is a simple GUI for installing packages on Linux.\n\nPlease click to install for install the specified package."
+        ),
     };
     let uninstall_msgs = match language {
         "en_US" => format!("Do you want to uninstall the package {}?", package_path),
@@ -77,7 +99,7 @@ pub fn welcome_screen(language: &str, package_manager: &str, package_path: &str,
             let uninstallmsg = Question::new(uninstall_msgs).show();
             match uninstallmsg {
                 Ok(Choice::Yes) => {
-                    uninstall_package(language, package_manager, &package_path);
+                    uninstall_package(language, package_manager, &package_path, its_arch);
                 }
                 Ok(Choice::No) | Ok(Choice::Cancel) => {
                     println!("Installation canceled");
@@ -90,7 +112,7 @@ pub fn welcome_screen(language: &str, package_manager: &str, package_path: &str,
             let installmsg = Question::new(msg_confirm).show();
             match installmsg {
                 Ok(Choice::Yes) => {
-                    install_package(language, package_manager, &package_path);
+                    install_package(language, package_manager, &package_path, its_arch);
                 }
                 Ok(Choice::No) | Ok(Choice::Cancel) => {
                     println!("Installation cancelled");
@@ -103,66 +125,84 @@ pub fn welcome_screen(language: &str, package_manager: &str, package_path: &str,
         }
     }
 
+    pub fn uninstall_package(
+        language: &str,
+        package_manager: &str,
+        package_path: &str,
+        its_arch: i32,
+    ) {
+        let mut _command = format!("");
+        if its_arch == 1 {
+            let mut _command = format!("{} -Rc {} --noconfirm", package_manager, package_path);
+        } else {
+            let mut _command = format!("{} remove {} -y", package_manager, package_path);
 
-
-
-pub fn uninstall_package(language: &str, package_manager: &str, package_path: &str) {
-    let command = format!("{} remove -y {}", package_manager, package_path);
-    let output = std::process::Command::new("pkexec")
-        .arg("sh")
-        .arg("-c")
-        .arg(command)
-        .output()
-        .expect("Failed to execute command");
-    if output.status.success() {
-        let success_message = match language {
-            "en_US" => "Package uninstalled successfully!",
-            "fr_FR" => "Paquet desinstallé avec succès !",
-            "es_ES" => "¡Paquete desinstalado con éxito!",
-            _ => "Package uninstalled successfully!",
-        };
-        let success_message_obj = Message::new(success_message);
-        let _ = Zenity::new().show_message(&success_message_obj);
-    } else {
-        let error_message = match language {
-            "en_US" => "Failed to uninstall package!",
-            "fr_FR" => "Échec de l'desinstaller du paquet !",
-            "es_ES" => "¡Error al desinstalar el paquete!",
-            _ => "Failed to uninstall package!",
-        };
-        let error_message_obj = Message::new(error_message);
-        let _ = Zenity::new().show_message(&error_message_obj);
+            let output = std::process::Command::new("pkexec")
+                .arg("sh")
+                .arg("-c")
+                .arg(_command)
+                .output()
+                .expect("Failed to execute command");
+            if output.status.success() {
+                let success_message = match language {
+                    "en_US" => "Package uninstalled successfully!",
+                    "fr_FR" => "Paquet desinstallé avec succès !",
+                    "es_ES" => "¡Paquete desinstalado con éxito!",
+                    _ => "Package uninstalled successfully!",
+                };
+                let success_message_obj = Message::new(success_message);
+                let _ = Zenity::new().show_message(&success_message_obj);
+            } else {
+                let error_message = match language {
+                    "en_US" => "Failed to uninstall package!",
+                    "fr_FR" => "Échec de l'desinstaller du paquet !",
+                    "es_ES" => "¡Error al desinstalar el paquete!",
+                    _ => "Failed to uninstall package!",
+                };
+                let error_message_obj = Message::new(error_message);
+                let _ = Zenity::new().show_message(&error_message_obj);
+            }
+        }
     }
-}
-pub fn install_package(language: &str, package_manager: &str, package_path: &str) {
-    let update_pkgs = format!("{} update", package_manager);
-    let command = format!("{} install -y {}", package_manager, package_path);
-    let output = std::process::Command::new("pkexec")
-        .arg("sh")
-        .arg("-c")
-        .arg(update_pkgs)
-        .arg("&&")
-        .arg(command)
-        .output()
-        .expect("Failed to execute command");
-    if output.status.success() {
-        let success_message = match language {
-            "en_US" => "Package installed successfully!",
-            "fr_FR" => "Paquet installé avec succès !",
-            "es_ES" => "¡Paquete instalado con éxito!",
-            _ => "Package installed successfully!",
-        };
-        let success_message_obj = Message::new(success_message);
-        let _ = Zenity::new().show_message(&success_message_obj);
-    } else {
-        let error_message = match language {
-            "en_US" => "Failed to install package!",
-            "fr_FR" => "Échec de l'installation du paquet !",
-            "es_ES" => "¡Error al instalar el paquete!",
-            _ => "Failed to install package!",
-        };
-        let error_message_obj = Message::new(error_message);
-        let _ = Zenity::new().show_message(&error_message_obj);
-    }
+    pub fn install_package(
+        language: &str,
+        package_manager: &str,
+        package_path: &str,
+        its_arch: i32,
+    ) {
+        if its_arch == 1 {
+            let _update_pkgs = format!("{} -Sy", package_manager);
+            let _command = format!("{} -S {} --noconfirm", package_manager, package_path);
+        } else {
+            let _update_pkgs = format!("{} update", package_manager);
+            let _command = format!("{} install -y {}", package_manager, package_path);
+            let output = std::process::Command::new("pkexec")
+                .arg("sh")
+                .arg("-c")
+                .arg(_update_pkgs)
+                .arg("&&")
+                .arg(_command)
+                .output()
+                .expect("Failed to execute command");
+            if output.status.success() {
+                let success_message = match language {
+                    "en_US" => "Package installed successfully!",
+                    "fr_FR" => "Paquet installé avec succès !",
+                    "es_ES" => "¡Paquete instalado con éxito!",
+                    _ => "Package installed successfully!",
+                };
+                let success_message_obj = Message::new(success_message);
+                let _ = Zenity::new().show_message(&success_message_obj);
+            } else {
+                let error_message = match language {
+                    "en_US" => "Failed to install package!",
+                    "fr_FR" => "Échec de l'installation du paquet !",
+                    "es_ES" => "¡Error al instalar el paquete!",
+                    _ => "Failed to install package!",
+                };
+                let error_message_obj = Message::new(error_message);
+                let _ = Zenity::new().show_message(&error_message_obj);
+            }
+        }
     }
 }
